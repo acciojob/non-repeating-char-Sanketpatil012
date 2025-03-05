55d17c520d3359ca1004a6fea7dec58621e88c8b
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-05 10:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+
+	let mapContainer =  new Map()
+
+	 for(chars of str){
+		mapContainer.set(chars ,  (mapContainer.get(chars) || 0 ) + 1)
+	 }
+
+	 for(chars of str){
+		  if(mapContainer.get(chars) == 1){
+			   return chars 
+		  }
+	 }
+	return null 
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
